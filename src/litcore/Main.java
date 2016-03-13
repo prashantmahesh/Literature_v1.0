ba0d10972ac9efcd895f39a84c4550a00f0e132a
Prashant Mahesh <prashantmahesh@gmail.com>
2016-03-13 18:29:14 +0530

    Changed classpath configuration to Java 8

--- src/litcore/Main.java
+++ src/litcore/Main.java
@@ -3,6 +3,7 @@ package litcore;
 public class Main {
 	public static void main(String args[]){
 		Deck d = new Deck();
+		d.shuffle();
 		for(int i = 0;i< 48;i++){
 			System.out.println(d.get(i));
 		}
